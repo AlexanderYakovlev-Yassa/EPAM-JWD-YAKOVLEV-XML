791a12d7cb04d32daffb77272db4e2a99b47b44e
Alexander Yakovlev <sashayassa@gmail.com>
2020-03-14 11:45:45 +0300

    Add validator and its test

--- src/main/java/by/epam/jwd/yakovlev/xmlparser/validator/XSDValidator.java
+++ src/main/java/by/epam/jwd/yakovlev/xmlparser/validator/XSDValidator.java
@@ -1,36 +1,39 @@
 package by.epam.jwd.yakovlev.xmlparser.validator;
 
-import java.io.*;
+import by.epam.jwd.yakovlev.xmlparser.exception.XSDValidatorException;
+import org.xml.sax.SAXException;
+
 import javax.xml.XMLConstants;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
 import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
-
-import by.epam.jwd.yakovlev.xmlparser.exception.XSDValidatorException;
-import org.xml.sax.SAXException;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class XSDValidator {
 
-    private Schema schema = null;
-    private String language = XMLConstants.W3C_XML_SCHEMA_NS_URI;
-    private SchemaFactory factory = SchemaFactory.newInstance(language);
+    private static final String LANGUAGE = XMLConstants.W3C_XML_SCHEMA_NS_URI;
+    private static final SchemaFactory FACTORY = SchemaFactory.newInstance(LANGUAGE);
+    private static final String XSD_SCHEMA = "src\\main\\resources\\XSDSchema\\GemsXSD.xsd";
 
-    public void validateXML(String xmlFileName, String xsdFileName) throws XSDValidatorException {
-
+    public void validateXMLFile(InputStream XMLFileStream) throws XSDValidatorException {
+        System.out.println("XMLFileStream is null " + (XMLFileStream == null));
+        File schemaFile = new File(XSD_SCHEMA);
+        System.out.println("schemaFile " + (schemaFile.getAbsoluteFile()));
         try {
-            schema = factory.newSchema(new File(xsdFileName));
-            SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
-            saxParserFactory.setSchema(schema);
-            SAXParser parser = saxParserFactory.newSAXParser();
-        } catch (ParserConfigurationException e) {
-            throw new XSDValidatorException(xmlFileName + " config error: " + e.getMessage(), e);
-        } catch (SAXException e) {
-            throw new XSDValidatorException("SAX exception: " + e, e);
+            Schema schema = FACTORY.newSchema(schemaFile);
+            Validator validator = schema.newValidator();
+            System.out.println("validator " + (validator.hashCode()));
+            Source source = new StreamSource(XMLFileStream);
+            System.out.println("source " + (source.toString()));
+            validator.validate(source);
+            System.out.println("OK");
+        } catch (SAXException | IOException e) {
+            System.out.println("XML file is not valid because" + e.getMessage());
+            throw new XSDValidatorException("XML file is not valid because" + e.getMessage(), e);
         }
     }
 }
--- src/test/java/by/epam/jwd/yakovlev/xmlparser/validator/XSDValidatorTest.java
+++ src/test/java/by/epam/jwd/yakovlev/xmlparser/validator/XSDValidatorTest.java
@@ -4,22 +4,37 @@ import by.epam.jwd.yakovlev.xmlparser.exception.XSDValidatorException;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class XSDValidatorTest {
 
     private static final String FILE_XML = "src\\test\\testresources\\Gems.xml";
-    private static final String FILE_XSD_XML = "src\\test\\testresources\\GemsXSD.xsd";
 
     @Test
-    public void validateXMLTest() {
+    public void validateXMLFileTest() {
+
+        InputStream XMLFileStream = null;
+
+        try {
+            XMLFileStream = new FileInputStream(FILE_XML);
+        } catch (FileNotFoundException e) {
+            Assert.fail();
+        }
 
         XSDValidator validator = new XSDValidator();
 
+        boolean actualResult = false;
+        System.out.println("VALIDATOR " + (validator.hashCode()));
         try {
-            validator.validateXML(FILE_XML, FILE_XSD_XML);
+            validator.validateXMLFile(XMLFileStream);
+            actualResult = true;
         } catch (XSDValidatorException e) {
+            System.out.println(e.getMessage());
             Assert.fail();
         }
+
+        Assert.assertTrue(actualResult);
     }
 }
